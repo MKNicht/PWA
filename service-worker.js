750098a35fa1a53b6f855288235bd3ae8e993e5e
Kowen <gonsama.uk@gmail.com>
2024-03-10 00:43:58 +0800

    update

--- service-worker.js
+++ service-worker.js
@@ -18,26 +18,31 @@ var cacheFiles = [
     "image/physics.png"
 ];
 
-self.addEventListener("install", function(e) {
+self.addEventListener("install", function (e) {
     console.log("[Service Worker] Install");
     e.waitUntil(
-        caches.open(cacheName).then(function(cache){
+        caches.open(cacheName).then(function (cache) {
             console.log("[Service Worker] Caching files");
             return cache.addAll(cacheFiles);
         })
     );
 });
 
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', function (e) {
     e.respondWith(
-        caches.match(e.request).then(function(response) {
-            return response || fetch(e.request).then(function(response) {
-                let responseClone = response.clone();
-                caches.open(cacheName).then(function(cache) {
-                    cache.put(e.request, responseClone);
+        caches.match(e.request).then(function (cachedFile) {
+            if (cacheFiles) {
+                console.log("[Service Worker] Resource fetched from the cache for: " + e.request.url);
+                return cachedFile;
+            } else {
+                return fetch(e.request).then(function (response) {
+                    return caches.open(cacheName).then(function (cache){
+                        cache.put(e.request, response.clone());
+                        console.log("[Service Worker] Resource fetched and saved in the cache for: " + e.request.url);
+                        return response;
+                    });
                 });
-                return response;
-            });
+            }
         })
     );
 });
